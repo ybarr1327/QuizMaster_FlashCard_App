d806a3bd6d0037bc904660e07254fea868e382c5
ybarr1327 <ybarr015@cougars.csusm.edu>
2021-04-06 11:10:01 -0700

    create splash and home page

--- app/src/main/java/com/example/final_app/MainActivity.kt
+++ app/src/main/java/com/example/final_app/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.example.final_app
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val gotoFCActBtn = findViewById<Button>(R.id.GoToFCActBtn)
+
+
+        gotoFCActBtn.setOnClickListener {
+            startActivity(Intent(this, FlashcardActivity::class.java))
+        }
+
+
+
     }
 }
